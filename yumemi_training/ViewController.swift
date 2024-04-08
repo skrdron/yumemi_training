7fb7ab1b49880e7db84a5eba736b2c88c378e62a
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-04-08 14:08:44 +0900

    Initial Commit

---
+++ yumemi_training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi_training
+//
+//  Created by 櫻田龍之助 on 2024/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
