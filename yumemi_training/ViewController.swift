a5e11f2346f3d0aef809fcf06dfa4d3e5b469270
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-04-08 18:06:38 +0900

    session2 APIの使い方に苦戦しましたがアドバイスをもらい無事クリア。(スタイリングの違い少しあり)

--- yumemi_training/ViewController.swift
+++ yumemi_training/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func reloadButton(_ sender: Any) {
+        print("Button Clicked")
+        var result = YumemiWeather.fetchWeatherCondition()
+        let cloudyImage = UIImage(named:"iconmonstr-umbrella-1")
+        let rainyImage = UIImage(named:"iconmonstr-weather-1")
+        let sunnyImage = UIImage(named:"iconmonstr-weather-11")
+        if(result == "cloudy"){
+            imageView.image = cloudyImage
+        }else if(result == "rainy"){
+            imageView.image = rainyImage
+        }else if(result == "sunny"){
+            imageView.image = sunnyImage
+        }else{
+            print("ERROR")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
