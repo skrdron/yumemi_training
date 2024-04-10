9a2d3c8adcb8e9ddd846385bbad8288aad8a9c48
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-04-10 17:08:05 +0900

    session6完了

--- yumemi_training/FirstViewController.swift
+++ yumemi_training/FirstViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    var timer:Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
-        // Do any additional setup after loading the view.
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let nextVC = storyboard?.instantiateViewController(identifier: "SecondView")
+        self.present(nextVC!, animated: true, completion: nil)
     }
 }
