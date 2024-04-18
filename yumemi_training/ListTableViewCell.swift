112d78a3ccbe282246481fd3d20e9c0ea186a71c
ryunosukesakurada <ryunosuke.sakurada@rightcode.co.jp>
2024-04-18 12:08:10 +0900

    session13 途中

--- yumemi_training/ListTableViewCell.swift
+++ yumemi_training/ListTableViewCell.swift
@@ -7,20 +7,10 @@
 
 import UIKit
 
-class ListTableViewCell: UITableViewCell {
- 
+class ListTableViewCell:UITableViewCell{
     @IBOutlet weak var area: UILabel!
     @IBOutlet weak var weatherImage: UIImage!
     @IBOutlet weak var minTemperature: UILabel!
     @IBOutlet weak var maxTemperature: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
-
 }
